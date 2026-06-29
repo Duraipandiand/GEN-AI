9a1577b0b86bc00b5d192f3aa907efec37c5244d
root <root@SVDI-5122.amzetta.co.in>
2026-06-29 16:49:19 +0000

    Initial commit

---
+++ Day1_basics/python_if.py
@@ -0,0 +1,10 @@
+grade = input("enter the grade:")
+
+if grade == 'A':
+    print("Execllent")
+elif grade == 'B':
+    print("GOOD")
+elif grade == 'C':
+    print("AVERGAE")
+else:
+    print("POOR")
---
+++ Day1_basics/python_if1.py
@@ -0,0 +1,8 @@
+age =int(input("Enter Your Age:"))
+
+if age >= 60:
+    print("Your are Elder citizen eligible to Caste Vote")
+elif age >= 40:
+    print("your are younger citizen to caste vote")
+else:
+    print("Your not eligible to caste vote")
